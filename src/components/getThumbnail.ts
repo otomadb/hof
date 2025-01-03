c8c5c29c8050caf79127978e194c4c1711c65df1
SnO2WMaN <me@sno2wman.net>
2025-01-03 10:31:35 +0900

    mightbefix

--- src/components/getThumbnail.ts
+++ src/components/getThumbnail.ts
@@ -1,4 +1,6 @@
+import ky from "ky";
 import pseudoThumbnail from "../images/pseudo_thumbnail.jpg";
+import * as v from "valibot";
 
 export default async function (
   type: string,
@@ -14,15 +16,26 @@ export default async function (
         return pseudoThumbnail.src;
       }
 
-      const res = await fetch(
+      const res = await ky.get(
         new URL(
           `/video/${sourceId}`,
           "https://nicovideo-api-proxy.otomadb.com",
         ),
+        {
+          retry: 3,
+          throwHttpErrors: false,
+        },
       );
       if (!res.ok) return pseudoThumbnail.src;
 
-      return (await res.json())?.thumbnailUrl || pseudoThumbnail.src;
+      const a = await res.json();
+      const b = v.safeParse(v.object({ thumbnailUrl: v.string() }), a);
+      if (!b.success) {
+        console.error(b.issues);
+        return pseudoThumbnail.src;
+      }
+
+      return b.output.thumbnailUrl;
     }
     default:
       return pseudoThumbnail.src;
